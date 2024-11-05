3aa488caf933d6cfe86ca5efbc0f55ef32baffd5
Well <Well@DESKTOP-22KNEBQ>
2024-11-05 05:23:35 -0800

    first commit

---
+++ C2TC_VANAVIL/src/com/tnsif/ifet/dayone/IdentifersDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.ifet.dayone;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java world");
+
+	}
+
+}
