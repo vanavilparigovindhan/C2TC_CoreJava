619dd720b101608046bc2d97efd17d2820ea7911
Well <Well@DESKTOP-22KNEBQ>
2024-11-12 06:17:25 +0530

    second commit

---
+++ C2TC_VANAVIL/src/com/tnsif/ifet/dayone/IndentifersDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.ifet.dayone;
+
+public class IndentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 20; //starts with $
+		System.out.println("value of the numbervariable is : "+ $number);
+		String studentName="Aniket"; //use camelcase
+		System.out.println("value is : "+
+		studentName);
+
+	}
+
+}
